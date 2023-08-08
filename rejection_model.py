fa23278b98be1ea3c98c3a2169b8ed034e6522af
Ali Zia <ziaali455@gmail.com>
2023-08-08 18:53:17 -0400

    g

--- rejection_model.py
+++ rejection_model.py
@@ -3,10 +3,23 @@ import streamlit as st
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
-st.markdown("test")
+st.markdown("# Divibank Summer 2023 Model Demo - Ali Zia")
+st.markdown("## Rejection Model:")
 loaded_model = pickle.load(open("model.pkl", 'rb'))
-raw_data = {'name': ['Bobs Burgers'], 'age': [3,], 'growth_overall': [-24.56]}  
+company_name = st.text_input('Client Name')
+company_age = st.text_input('Company Age (Years)')
+company_growth = st.text_input('Overall Growth (%)')
+
 #inputs: 
-data = pd.DataFrame(raw_data)
-data_scaled = scaler.fit_transform(data.drop(columns=['name']))
-st.markdown(loaded_model.predict(data_scaled))
+if st.button("Generate Result"):
+    raw_data = {'name': [company_name], 'age': [int(company_age)], 'growth_overall': [float(company_growth)]}  
+    data = pd.DataFrame(raw_data)
+    data_scaled = scaler.fit_transform(data.drop(columns=['name']))
+    st.markdown("### Approved?: ")
+    # if(loaded_model.predict(data_scaled)==False):
+    #     st.markdown("Not Approved")
+    # else:
+    #     st.markdown("Approved")
+    st.markdown(loaded_model.predict(data_scaled))
+
+
