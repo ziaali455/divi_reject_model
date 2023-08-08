eda40311bdb0f4cb94ce782347c3255405f993c4
Ali Zia <ziaali455@gmail.com>
2023-08-08 18:16:51 -0400

    stuff

--- rejection_model.py
+++ rejection_model.py
@@ -1,4 +1,12 @@
 import pickle
 import streamlit as st
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 st.markdown("test")
 loaded_model = pickle.load(open("model.pkl", 'rb'))
+raw_data = {'name': ['Bobs Burgers'], 'age': [3,], 'growth_overall': [-24.56]}  
+#inputs: 
+data = pd.DataFrame(raw_data)
+data_scaled = scaler.fit_transform(data.drop(columns=['name']))
+st.markdown(loaded_model.predict(data_scaled))
