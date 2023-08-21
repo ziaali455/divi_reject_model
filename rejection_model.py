95b70055a8b8ec7d3d8e08fe3945b347419e5b9a
Ali Zia <ziaali455@gmail.com>
2023-08-21 17:13:10 -0400

    fin

--- rejection_model.py
+++ rejection_model.py
@@ -1,15 +1,18 @@
 import pickle
 import streamlit as st
 import pandas as pd
+import sklearn
+from sklearn.preprocessing import StandardScaler
 
 st.markdown("# Divibank Summer 2023 Rejection Model Demo - Ali Zia")
 st.markdown("## Rejection Model:")
-loaded_rejection_model = pickle.load(open("model.pkl", 'rb'))
+loaded_rejection_model = pickle.load(open("model0.pkl", 'rb'))
 company_name = st.text_input('Client Name')
 company_age = st.text_input('Company Age (Years)')
 company_growth = st.text_input('Overall Growth (%)')
 
 if st.button("Generate Result #1"):
+    scaler = StandardScaler()
     raw_data = {'name': [company_name], 'age': [int(company_age)], 'growth_overall': [float(company_growth)]}  
     data = pd.DataFrame(raw_data)
     data_scaled = scaler.fit_transform(data.drop(columns=['name']))
@@ -18,7 +21,7 @@ if st.button("Generate Result #1"):
 
 st.markdown("# Divibank Summer 2023 Loan Plan Model Demo - Ali Zia")
 st.markdown("## Loan Model (Interest Rate):")
-loaded_loan_model = pickle.load(open("prelim_data.pkl", 'rb'))
+loaded_loan_model = pickle.load(open("model1.pkl", 'rb'))
 name = st.text_input('Client Name',key="1")
 age = st.text_input('Company Age (Years)',key="11")
 gross = st.text_input('Gross Revenue',key="111")
@@ -51,11 +54,7 @@ def rating_to_int(full_client_data, rating):
   full_client_data[rating] = full_client_data[rating].map(ratings)
   return full_client_data
 
-def dummy_ratings(full_client_data):
-  df_dummies = full_client_data.join(pd.get_dummies(full_client_data["Model Rating"], prefix="model_rating"))
-  print("Dummies: ", df_dummies.shape)
-  print("full_client_data: ", full_client_data.shape)
-  return df_dummies
+
 
 
 def eval_dummies(data):
@@ -65,9 +64,8 @@ if st.button("Generate Result #2"):
    raw_data = {'Age (Years)': [age], '01_Gross_revenue': [gross], 'Model Rating': [rating]}
    raw_data = pd.DataFrame.from_dict(raw_data)
    raw_data = rating_to_int(raw_data, 'Model Rating')
-   st.markdown(raw_data)
    st.markdown("### Interest Rate: ")
-   st.markdown(eval_dummies(dummy_ratings(raw_data)))
+   st.markdown(str(eval_dummies(raw_data))[1:-1] + "%")
 
 
 
